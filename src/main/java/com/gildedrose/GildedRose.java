52f0174eb7d0bddbe750ff773b25678b27013069
梁仟 <liangqian0723@gmail.com>
2020-02-13 14:23:36 +0800

    feature envy quality的计算和sellIn的计算都是属于item的引用，迁移到item里面处理 move method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,53 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
-            if (!items[i].name.equals(ItemName.AGED_BRIE.getName())
-                    && !items[i].name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
-
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(ItemName.SULFURAS_HAND_OF_RAGNAROS.getName())) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else{
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            // 处理保质期
-            items[i].setSellIn();
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(ItemName.AGED_BRIE.getName())) {
-                    if (!items[i].name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(ItemName.SULFURAS_HAND_OF_RAGNAROS.getName())) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else{
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].calcQuality();
         }
     }
 
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -25,4 +25,53 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void calcQuality() {
+        if (!name.equals(ItemName.AGED_BRIE.getName())
+                && !name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
+            if (quality > 0) {
+                if (!name.equals(ItemName.SULFURAS_HAND_OF_RAGNAROS.getName())) {
+                    quality = quality - 1;
+                }
+            }
+        } else{
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+        // 处理保质期
+        setSellIn();
+
+        if (sellIn < 0) {
+            if (!name.equals(ItemName.AGED_BRIE.getName())) {
+                if (!name.equals(ItemName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.getName())){
+                    if (quality > 0) {
+                        if (!name.equals(ItemName.SULFURAS_HAND_OF_RAGNAROS.getName())) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else{
+                    quality = quality - quality;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
 }
